6e89177ff3f762e4a8fd33cb1fb52d229df2a6b7
Kau <karoltvbl@gmail.com>
2025-08-04 20:50:24 -0300

    feat: add custom categories

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 async function run() {
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 
+    const categorias = await fazerPergunta("Me fale sobre as categorias de um deteriminado destino que deseja visualizar: ");
+
     const prompt = await fazerPergunta("Me fale sobre o destino que deseja conhecer: ");
 
     const parts = [
-        {text: "Você é o chatbot de um site que vende pacotes de viagem. Ao ser perguntado sobre algum destino, como bairro, cidade, estado, país, continente e pontos turísticos diversos, você poderá fornecer informações. Caso seja perguntado sobre algo que não ter relação com viagem e turismo, informe que não poder responder a essa dúvida.\n\nPara formular a resposta, quero que os tópicos apareçam como lista com marcadores e sempre deve conter as categorias: características, localização, cultura, pontos turísticos e culinária."},
-        {text: `input: me fale sobre o destino ${prompt}`},
+        {text: "Você é o chatbot de um site que vende pacotes de viagem. Ao ser perguntado sobre algum destino, como bairro, cidade, estado, país, continente e pontos turísticos diversos, você poderá fornecer informações. Caso seja perguntado sobre algo que não ter relação com viagem e turismo, informe que não poder responder a essa dúvida.\n\nPara formular a resposta, quero que os tópicos apareçam como lista com marcadores e sempre deve conter as categorias que forem solicitadas no momento da pergunta. Alguns exemplos de categorias: características, localização, cultura, pontos turísticos, clime, dicas, como chegar, culinária, curiosidades."},
+        {text: `input: me fale sobre ${categorias} o destino ${prompt}`},
         {text: "output: "}
     ];
     const result = await model.generateContent({
