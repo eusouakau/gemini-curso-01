4e156bf351a3e4a16dae670156bdd79ef386cddd
Kau <karoltvbl@gmail.com>
2025-08-04 20:35:42 -0300

    refac:  improves ai response

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ async function run() {
 
   const prompt = await fazerPergunta("Me fale sobre o destino que deseja conhecer: ");
 
-  const parts = [
-    {text: "Você é um chatbot de um site que vende pacotes de viagens. Ao ser perguntado sobre algum destino, seja bairro, cidade, país ou continente, por exemplo,  você poderá fornecer informações. Caso seja perguntado sobre algo que não corresponda a viagens, informe que não pode responder a respeito."},
-    {text: `input: me fale sobre o destino ${prompt}`},
-    {text: "output: "},
-];
+    const parts = [
+      {text: "Você é o chatbot de um site que vende pacotes de viagem. Ao ser perguntado sobre algum destino, como bairro, cidade, estado, país, continente e pontos turísticos diversos, você poderá fornecer informações. Caso seja perguntado sobre algo que não ter relação com viagem e turismo, informe que não poder responder a essa dúvida.\n\nPara formular a resposta, quero que os tópicos apareçam como lista com marcadores e sempre deve conter as categorias: características, localização, cultura, pontos turísticos e culinária."},
+      {text: `input: me fale sobre o destino ${prompt}`},
+      {text: "output: "}
+    ];
   const result = await model.generateContent({
     contents: [{ role: "user", parts }]}
   );
