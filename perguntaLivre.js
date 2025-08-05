ab88b7f93ddcda07fca5387e81aa7f60a2e88bbe
Kau <karoltvbl@gmail.com>
2025-08-04 21:32:22 -0300

    feat:  adds token count

--- perguntaLivre.js
+++ perguntaLivre.js
@@ -11,10 +11,20 @@ const prompt = await fazerPergunta("Me faça um pergunta sobre um determinado de
         { text: `input: ${ prompt }` },
         { text: "output: " }
     ];
-    const result = await model.generateContent({
-        contents: [{ role: "user", parts }]}
+
+    const requisicao = (
+        { contents: [{ role: "user", parts }]}
     );
+ 
+    const result = await model.generateContent(requisicao);
+
+    const totalTokensEntrada = await model.countTokens(requisicao);
+    console.log(`\nTotal tokens de entrada:  ${totalTokensEntrada.totalTokens}\n`)
+
     const response = await result.response;
     const text = response.text();
     console.log(text);
+
+    const totalTokensSaida = await model.countTokens(text);
+    console.log(`\nTotal tokens de saída:  ${totalTokensSaida.totalTokens}\n`)
 }
